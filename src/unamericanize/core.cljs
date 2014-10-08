c12b0b02cd5787f5d2933d5ed0f3d4732e816362
Tom Hancock <thomas.ian.hancock@gmail.com>
2014-10-08 15:18:51 -0600

    Replace american food terms with their english equivalent

--- src/unamericanize/core.cljs
+++ src/unamericanize/core.cljs
@@ -1,9 +1,10 @@
 (ns unamericanize.core
-  (:require [clojure.string :refer [replace]]))
+  (:require [clojure.string :refer [replace upper-case capitalize lower-case split join]]))
 
-(enable-console-print!)
-
-(println "loaded!")
+(def conversions
+  {"zucchini" "aubergine"
+   "scallion" "spring onion"
+   "cilantro" "coriander"})
 
 (defn walk-nodes
   [node f]
@@ -11,6 +12,25 @@
     (doseq [n (array-seq (.-childNodes node))] (walk-nodes n f))
     (f node)))
 
+(defn capitalization
+  [term]
+  (cond
+   (= (upper-case term) term) upper-case
+   (= (capitalize term) term) capitalize
+   :else                      lower-case))
+
+(defn replace-match
+  [match to]
+  (let [tokenized-to (split to #" ")]
+    (join " " (map (capitalization match) tokenized-to))))
+
+(defn unamericanize
+  [input-text conversion-map]
+  (reduce
+   (fn [val [from to]] (.replace val (js/RegExp. from "ig") (fn [match] (replace-match match to))))
+   input-text
+   conversion-map))
+
 (walk-nodes
  js/document.body
- (fn [n] (set! (.-textContent n) (replace (.-textContent n) #"a" "fantabulouso"))))
+ (fn [n] (set! (.-textContent n) (unamericanize (.-textContent n) conversions))))
